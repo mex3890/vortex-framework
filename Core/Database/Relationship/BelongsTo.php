c1069843a4fc4790020f389b7288a9f96c1b4f9f
Caio Albuquerque <abcv.tenorio@gmail.com>
2023-01-05 09:16:15 -0300

    Fix optional column parameters

--- Core/Database/Relationship/BelongsTo.php
+++ Core/Database/Relationship/BelongsTo.php
@@ -6,13 +6,16 @@ use Core\Database\Query\SelectBuilder;
 
 class BelongsTo extends Relation
 {
+    /**
+     * @return SelectBuilder
+     */
     public function mount(): SelectBuilder
     {
-        if (is_null($this->secondary_column)) {
-            $this->secondary_column = 'id';
+        if (is_null($this->main_column)) {
+            $this->main_column = 'id';
         }
 
         $select_builder = new SelectBuilder($this->secondary_model->table, '*', $this->secondary_model);
-        return $select_builder->where($this->secondary_column, $this->main_id)->limit(1);
+        return $select_builder->where($this->main_column, $this->main_id)->limit(1);
     }
 }
--- Core/Database/Relationship/BelongsToMany.php
+++ Core/Database/Relationship/BelongsToMany.php
@@ -7,12 +7,11 @@ use Core\Helpers\StringFormatter;
 
 class BelongsToMany extends Relation
 {
+    /**
+     * @return SelectBuilder
+     */
     public function mount(): SelectBuilder
     {
-        if (is_null($this->secondary_column)) {
-            $this->secondary_column = 'id';
-        }
-
         $singular_main_table = StringFormatter::singularize($this->main_model->table);
         $singular_secondary_table = StringFormatter::singularize($this->secondary_model->table);
 
@@ -23,20 +22,25 @@ class BelongsToMany extends Relation
             sort($tables);
             $pivot_table_name = "$tables[0]_$tables[1]";
         }
+        if (is_null($this->main_column)) {
+            $this->main_column = $singular_main_table . '_id';
+        }
 
-        $main_foreign_key = $singular_main_table . '_id';
-        $secondary_foreign_key = $singular_secondary_table . '_id';
+        if (is_null($this->secondary_column)) {
+            $this->secondary_column = $singular_secondary_table . '_id';
+        }
 
         $select_builder = new SelectBuilder($this->secondary_model->table, '*', $this->secondary_model);
+
         return $select_builder
             ->innerJoin(
                 $pivot_table_name,
-                "{$pivot_table_name}.$secondary_foreign_key",
+                "{$pivot_table_name}.$this->secondary_column",
                 "{$this->secondary_model->table}.id"
             )
             ->innerJoin(
                 $this->main_model->table,
-                "$pivot_table_name.$main_foreign_key",
+                "$pivot_table_name.$this->main_column",
                 "{$this->main_model->table}.id"
             )->where('id', $this->main_id, '=', $this->main_model->table);
     }
