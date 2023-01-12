1a5d22ad330797b5e8a7872525e401a3ca91720b
Caio Albuquerque <abcv.tenorio@gmail.com>
2023-01-12 00:19:08 -0300

    Update Request class

--- Core/Request/Request.php
+++ Core/Request/Request.php
@@ -4,20 +4,107 @@ namespace Core\Request;
 
 class Request
 {
-    private array $parameters = [];
-    private const SERVER_HOST = 'HTTP_HOST';
-    private const HTTP_ORIGIN = 'HTTP_ORIGIN';
-    private const SERVER_NAME = 'SERVER_NAME';
-    private const SERVER_PORT = 'SERVER_PORT';
-    private const SERVER_PROTOCOL = 'SERVER_PROTOCOL';
-    private const SCRIPT_FILENAME = 'SCRIPT_FILENAME';
-    private const REQUEST_METHOD = 'REQUEST_METHOD';
-    private const REQUEST_URI = 'REQUEST_URI';
-    private const APP_URL = 'APP_URL';
+    public string $method;
+    public string $host;
+    public string $origin;
+    public string $server_port;
+    public string $protocol;
+    public string $filename;
+    public string $url;
+    public string $uri;
+    public ?array $parameters;
+    public ?array $attributes;
+    public ?array $files;
+    public ?array $_SERVER;
+    public ?array $_COOKIE;
+    public ?array $_SESSION;
+    public ?array $_ENV;
+
+    public const SERVER_HOST = 'HTTP_HOST';
+    public const HTTP_ORIGIN = 'HTTP_ORIGIN';
+    public const SERVER_PORT = 'SERVER_PORT';
+    public const SERVER_PROTOCOL = 'SERVER_PROTOCOL';
+    public const SCRIPT_FILENAME = 'SCRIPT_FILENAME';
+    public const REQUEST_METHOD = 'REQUEST_METHOD';
+    public const REQUEST_URI = 'REQUEST_URI';
+    public const APP_URL = 'APP_URL';
+
+    public function __construct()
+    {
+        $this->setHost();
+        $this->setOrigin();
+        $this->setServerPort();
+        $this->setAttributes();
+        $this->setMethod();
+        $this->setProtocol();
+        $this->setFilename();
+        $this->setUrl();
+        $this->setUri();
+        $this->setFiles();
+        $this->setServer();
+        $this->setSession();
+        $this->setCookie();
+        $this->setEnv();
+    }
+
+    public function setServer(): void
+    {
+        $this->_SERVER = $_SERVER ?? null;
+    }
+
+    public function server(): ?array
+    {
+        return $this->_SERVER;
+    }
+
+    public function setCookie(): void
+    {
+        $this->_COOKIE = $_COOKIE ?? null;
+    }
+
+    public function cookie(): ?array
+    {
+        return $this->_COOKIE;
+    }
+
+    public function setSession(): void
+    {
+        $this->_SESSION = $_SESSION ?? null;
+    }
+
+    public function session(): ?array
+    {
+        return $this->_SESSION;
+    }
+
+    public function setEnv(): void
+    {
+        $this->_ENV = $_ENV ?? null;
+    }
+
+    public function env(): ?array
+    {
+        return $this->_ENV;
+    }
+
+    public function setAttributes(): void
+    {
+        $attributes = $_POST;
+
+        if (isset($atributes['vortex_redirect'])) {
+            unset($attributes['vortex_redirect']);
+        }
+
+        if (isset($attributes['vortex_method'])) {
+            unset($attributes['vortex_method']);
+        }
+
+        $this->attributes = $attributes;
+    }
 
     public function attributes(): array
     {
-        return $_POST;
+        return $this->attributes;
     }
 
     public function setParameters(array $parameters): void
@@ -30,53 +117,121 @@ class Request
         return $this->parameters;
     }
 
-    public function host(): array
+    public function setHost(): void
     {
-        return $_SERVER[self::SERVER_HOST];
+        $this->host = $_SERVER[self::SERVER_HOST];
     }
 
-    public function origin()
+    public function host(): string
     {
-        return $_SERVER[self::HTTP_ORIGIN];
+        return $this->host;
     }
 
-    public function server()
+    public function setOrigin(): void
     {
-        return $_SERVER[self::SERVER_NAME];
+        if (isset($_SERVER[self::HTTP_ORIGIN])) {
+            $this->origin = $_SERVER[self::HTTP_ORIGIN];
+        }
     }
 
-    public function serverPort()
+    public function origin(): string
     {
-        return $_SERVER[self::SERVER_PORT];
+        return $this->origin;
     }
 
-    public function protocol()
+    public function setServerPort(): void
     {
-        return $_SERVER[self::SERVER_PROTOCOL];
+        $this->server_port = $_SERVER[self::SERVER_PORT];
     }
 
-    public function filename()
+    public function serverPort(): string
     {
-        return $_SERVER[self::SCRIPT_FILENAME];
+        return $this->server_port;
     }
 
-    public function method()
+    public function setProtocol(): void
     {
-        return $_SERVER[self::REQUEST_METHOD];
+        $this->protocol = $_SERVER[self::SERVER_PROTOCOL];
     }
 
-    public function uri()
+    public function protocol(): string
     {
-        return $_SERVER[self::REQUEST_URI];
+        return $this->protocol;
     }
 
-    public function url()
+    public function setFilename(): void
     {
-        return $_SERVER[self::APP_URL];
+        $this->filename = $_SERVER[self::SCRIPT_FILENAME];
+    }
+
+    public function filename(): string
+    {
+        return $this->filename;
+    }
+
+    public function setMethod(): void
+    {
+        $this->method = $_SERVER[self::REQUEST_METHOD];
+    }
+
+    public function method(): string
+    {
+        return $this->method;
+    }
+
+    public function setUri(): void
+    {
+        $this->uri = $_SERVER[self::REQUEST_URI];
+    }
+
+    public function uri(): string
+    {
+        return $this->uri;
+    }
+
+    public function setUrl(): void
+    {
+        $this->url = $_SERVER[self::APP_URL];
+    }
+
+    public function url(): string
+    {
+        return $this->url;
+    }
+
+    public function setFiles(): void
+    {
+        $this->files = $_FILES ?? null;
     }
 
     public function files(): array
     {
-        return $_FILES;
+        return $this->files;
+    }
+
+    /**
+     * @param string $attribute_name
+     * @return mixed|null
+     */
+    public function getAttribute(string $attribute_name): mixed
+    {
+        if (isset($this->attributes[$attribute_name])) {
+            return $this->attributes[$attribute_name];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $parameter_name
+     * @return mixed|null
+     */
+    public function getParameter(string $parameter_name): mixed
+    {
+        if (isset($this->parameters[$parameter_name])) {
+            return $this->parameters[$parameter_name];
+        }
+
+        return null;
     }
 }
